76a87c735419595f3547b514d35b752c5df70eb3
Milos Zivadinovic <mzdv@outlook.com>
2014-12-18 23:19:49 +0100

    Started additional implementations.

--- server.js
+++ server.js
@@ -5,12 +5,14 @@
 var net = require("net");
 var mapMonad = require("mapMonad");
 
-const DELIMETER = "\\/|\\|\\/"; //if you look closely, it spells out VNV, as in VNV Nation
+const DELIMETER = "|";
 
 var incomingData = '';
 var message = [];                          //parsing structure
 var valueContainer = [];                   //id holder
 var keyContainer = [];
+var port = process.env.port || 1389;
+var finishedConversions = [];
 
 net.createServer(function(socket) {
     socket.setEncoding("utf8");
@@ -24,14 +26,15 @@ net.createServer(function(socket) {
                 message = incomingData.split(DELIMETER);
                 id = Math.floor(Math.random() * 100);
 
-                switch (message[0]) {
+                switch (message[0]) {       //message 1 ip adresa konvertora, message 2 prvobitan broj, message 3 konacan broj, message 4 odabrana konv
                     case "DJE_SI_GRGA_DRUZE_STARI":     //registracija; korak 1
-                        mapMonad(keyContainer, valueContainer, socket.remoteAddress);
-
+                        mapMonad(keyContainer, valueContainer, socket.remoteAddress);   // dodati possible conversions; imas na forumu
                         socket.write("ZA_NAPLATU_TI_NE_MARI" + DELIMETER + valueContainer + "\n");  //odgovor mogucih klijenata; korak 2
                         break;
 
-                    case "VOZI_ME_ZA_SURCIN_PREKO_LEDINA":      //gotova konverzija; korak 3
+                    case "VOZI_ME_ZA_SURCIN_PREKO_LEDINA":          //gotova konverzija; korak 3
+                        finishedConversions.push(socket.remoteAddress + '|' + message[1] + '|' + message[2] + '|' + message[3] + '|' + message[4] + '\n');
+                        //ovo gore izmeni u write to file
                         socket.write("TAMO_ZIVI_MOJA_JEDINA\n");    //potvrda o primljenom kraju; ne mora, ali ajde; korak 4
                         break;
 
@@ -50,6 +53,6 @@ net.createServer(function(socket) {
         .on("error", function(error) {
             console.log(error.toString());
         })
-}).listen(1389);        //the only patriotic port in the world
+}).listen(port);
 
-console.log("Basenumber management server is operational.");
+console.log("Management server is operational.");
