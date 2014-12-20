1476196690815d2a003f3f3a18a63f93ce64cfdb
Milos Zivadinovic <mzdv@outlook.com>
2014-12-20 18:04:34 +0100

    Added "conversion" command error checking. Added checking for undefined.

--- client.js
+++ client.js
@@ -1,17 +1,18 @@
 /**
  * Created by Milos on 16.12.2014..
  */
-var readline = require('readline');
-var clc = require('cli-color');
+var readline = require("readline");
+var clc = require("cli-color");
+var _ = require("lodash");
+
 var startSequence = require('./startSequence');
 var regexContainer = require('./regexContainer');
 
+var possibleConversionTypes = ["10.16", "16.10", "8.4", "4.8", "2.10", "10.2", "5.7", "7.5"];
+
 var number;
-var conversions;
+var wantedConversion;
 var serverAddress;
-
-//var ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -48,9 +49,26 @@ rl
                     console.log(clc.redBright("Not a supported number!"));
                 break;
 
-            case "conversions":
-                conversions = message[1];
-                console.log(clc.greenBright("Entered available conversions: " + clc.yellowBright(conversions) + '\n'));
+            case "conversions": // TODO: setup_conversions which sets up the conversions FOR THIS SERVER
+                if(_.isUndefined(message[1])){   // TODO: include this in the above examples
+                    console.log(clc.greenBright("Possible conversions to choose from: \n"));
+                    console.log(clc.greenBright("1) 10 to 16\n"));
+                    console.log(clc.greenBright("2) 16 to 10\n"));
+                    console.log(clc.greenBright("3) 8 to 4\n"));
+                    console.log(clc.greenBright("4) 4 to 8\n"));
+                    console.log(clc.greenBright("5) 2 to 10\n"));
+                    console.log(clc.greenBright("6) 10 to 2\n"));
+                    console.log(clc.greenBright("7) 5 to 7\n"));
+                    console.log(clc.greenBright("8) 7 to 5\n"));
+                }
+                else if(regexContainer.conversionRegex.test(message[1])) {
+                    wantedConversion = possibleConversionTypes[message[1]];
+
+                    var dismemberedConversion = wantedConversion.split('.');
+                    console.log(clc.greenBright("Entered new conversion: " + clc.yellowBright(dismemberedConversion[0] + " to " + dismemberedConversion[1]) + '\n'));
+                }
+                else
+                    console.log(clc.redBright("Not a valid conversion!"));
                 break;
 
             case "quit":
@@ -61,12 +79,12 @@ rl
                 var conversion = startSequence(serverAddress, number, conversions);
 
                 console.log(clc.greenBright("Converted number is: ") + clc.blueBright(conversion.convertedNumber) + '\n' +
-                            clc.greenBright("Original number was: ") + clc.blueBright(number) + '\n' +
-                            clc.greenBright("Conversion was from base system to base system: ") + clc.blueBright(conversion.conversionType) + '\n' +
-                            clc.greenBright("Conversion done at server: ") + clc.blueBright(conversion.converterAddress) + '\n');
+                clc.greenBright("Original number was: ") + clc.blueBright(number) + '\n' +
+                clc.greenBright("Conversion was from base system to base system: ") + clc.blueBright(conversion.conversionType) + '\n' +
+                clc.greenBright("Conversion done at server: ") + clc.blueBright(conversion.converterAddress) + '\n');
                 break;
 
-            default:            // TO-DO: multiple types of errors defined by a random variable
+            default:            // TODO: multiple types of errors defined by a random variable
                 console.log(clc.redBright("I'm sorry Dave, I'm afraid I can't do that.\n"));
                 break;
         }
--- regexContainer.js
+++ regexContainer.js
@@ -4,7 +4,7 @@
 var regexContainer = {
     ipRegex: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
     numberRegex: /^\d+$/,
-    conversionRegex: ""
+    conversionRegex: /^[1-8]$/
 };
 
 module.exports = regexContainer;
